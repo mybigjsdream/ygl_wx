07a7cdd7569b2cd63e5b2d441218f5f851b6aa9c
shanyu-e <554816284@qq.com>
2016-10-16 21:03:57 +0800

    ...

--- src/conf.py
+++ src/conf.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
+from wechat_sdk import WechatConf
+
 
 appname = "cms_spider_video"
 token = "dengjing"
@@ -14,12 +16,21 @@ logging.basicConfig(
 root_logger = logging.getLogger()
 
 
+wx_conf = WechatConf(
+    token='your_token',
+    appid='your_appid',
+    appsecret='your_appsecret',
+    encrypt_mode='safe',
+    encoding_aes_key='your_encoding_aes_key'
+)
+
+
 # 数据库配置
 # DBSTR = 'sqlite:///../db/test.db'
-DBSTR = 'sqlite:////data1/you-get-video/db/you-get.db'
-DBARGS = {'echo': False, 'echo_pool': False, 'encoding': 'UTF-8'}
+# DBSTR = 'sqlite:////data1/you-get-video/db/you-get.db'
+# DBARGS = {'echo': False, 'echo_pool': False, 'encoding': 'UTF-8'}
 
 # HOST_IP = '172.16.7.21'
 HOST_IP = '0.0.0.0'
-# HOST_PORT = '8888'
-HOST_PORT = '80'
+HOST_PORT = '8888'
+# HOST_PORT = '80'
