2421f35320b21eacde9594a62db05b9a4703fe21
shanyu-e <554816284@qq.com>
2016-11-14 12:56:10 +0800

    修改逻辑

--- src/model.py
+++ src/model.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
+import datetime
 from conf import mongo_str, root_logger, wechat
 from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
 import requests
 
 
@@ -26,20 +26,12 @@ def insert_new_wx_user(_id, data, doctor_id):
         'nickname': base_doctor_data['doctorName'],
         'headimgurl': base_doctor_data['avatar']
     }
-    try:
-        ret = db.get_db().wx_user.insert({
-            '_id': _id,
-            'data': data,
-            'doctor_data': doctor_data
-        })
-    except DuplicateKeyError:
-        root_logger.info(u"插入insert_new_wx_user重复")
-        ret = db.get_db().wx_user.update_one(
-            {'_id': _id},
-            {
-                '$set': {'doctor_data': doctor_data}
-            }
-        )
+    ret = db.get_db().wx_user.insert({
+        'u_id': _id,
+        'data': data,
+        'doctor_data': doctor_data,
+        'createAt': datetime.datetime.now()
+    })
     root_logger.info(u"插入insert_new_wx_user")
     root_logger.info(ret)
 
